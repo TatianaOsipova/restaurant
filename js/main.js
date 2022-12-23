7190788bdd1cb45d8e20835b8cbe6c39593c02df
Tatiana Osipova <otv120780@gmail.com>
2022-12-23 15:38:36 -0500

    feat: add id

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    $('.photo__slider').slick({
+    $('.comment__slider-slide').slick({
         prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt="arrow-left"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt="arrow-right"></button>',
 
