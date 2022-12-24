69d83e39f65aec947c3b73e1e79e1683f851e11c
Tatiana Osipova <otv120780@gmail.com>
2022-12-24 11:11:04 -0500

    fix: delete file js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-    $('.comment__slider-slide').slick({
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt="arrow-left"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt="arrow-right"></button>',
-
-    });
-
-});
