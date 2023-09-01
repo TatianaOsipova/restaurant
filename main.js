d07fd1ed4a086a5544358147b9194448555b2563
Tatiana Osipova <otv120780@gmail.com>
2023-09-01 10:49:03 -0400

    feat: correct burger menu

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
-// const burger = document.querySelector('#burger');
-// const menu = document.querySelector('#menu');
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
 
-// burger.addEventListener('click', () => {
-//     menu.classList.toggle('disp');
-// });
-
-const menuBtn = document.querySelector('.menu__btn');
-const menuMobile = document.querySelector('.header__menu-list');
-
-menuBtn.addEventListener('click', () => {
-  menuMobile.classList.toggle('menu--open');
+burger.addEventListener('click', () => {
+    menu.classList.toggle('disp');
 });
+
+
 
 
